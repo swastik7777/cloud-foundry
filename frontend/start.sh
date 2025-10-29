72054307e4629acfea6c7cd9f0151448d35ac771
swastik7777 <126863868+swastik7777@users.noreply.github.com>
2025-10-29 13:54:27 +0530

    Add missing newlines to start.sh

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 envsubst '$PORT' < /home/vcap/app/nginx/conf/nginx.conf.template > /home/vcap/app/nginx/conf/nginx.conf
 nginx -p /home/vcap/app/nginx -c /home/vcap/app/nginx/conf/nginx.conf
+
+
+
